360017022aad474b8b99c5cc5da45d21defb5378
hafeez741 <98201184+hafeez741@users.noreply.github.com>
2023-03-06 06:12:53 +0300

    Update run.sh

--- assignment10/run.sh
+++ assignment10/run.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env/ bash
+#!/usr/bin/env bash
 echo $1 > access
 gcloud compute instances create instancex --project=$2 --machine-type=e2-medium --network-interface=network=mycustvpc --zone=us-central1-a --acccess-token-file=access
